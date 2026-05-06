d3491f23531a29580d23676b318e9b196767d9d3
Romulus162 <roman.badier@gmail.com>
2026-05-06 04:32:00 +0200

    mfd themes

---
+++ lua/plugins/mfd.lua
@@ -0,0 +1,12 @@
+return {
+	"kungfusheep/mfd.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		vim.cmd("colorscheme mfd-stealth")
+
+		vim.api.nvim_set_hl(0, "NeoTreeGitUntracked", {
+			fg = "000000",
+		})
+	end,
+}
